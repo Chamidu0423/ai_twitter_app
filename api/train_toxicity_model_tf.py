f3931ab62e34892a2612ca44d78c21bd9f110bb5
Chamidu Dilshan <chamidudilshan0423@gmail.com>
2025-11-18 01:22:51 +0530

    Add data loading functionality to train toxicity model

--- api/train_toxicity_model_tf.py
+++ api/train_toxicity_model_tf.py
@@ -7,3 +7,19 @@ import os
 print("---AI Tools Imported Successfully---")
 print(f"Tensorflow Version: {tf.__version__}")
 
+print("Loading Data Set...")
+try:
+    data_path = 'toxic_data.csv'
+
+    df = pd.read_csv(data_path)
+
+    print("Data Set Loaded Successfully. Here are the  first 5 rows:")
+    print(df.head())
+
+except FileNotFoundError:
+    print(f"Error: Data Set not found at {data_path}")
+    exit()
+
+except Exception as e:
+    print(f"Error loading Data Set: {e}")
+    exit()
